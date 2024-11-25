35ca6c4516a040f4b57285008ff95b0bb2a03dea
guruprasad <guruprasadpati2@gmail.com>
2024-11-25 07:36:17 -0500

    Changed message

---
+++ Driver_Quit.java
@@ -0,0 +1,11 @@
+package Tata_Power_Electric.Connection_for_New_Residential_Customer;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Driver_Quit {
+	public void DRQ(WebDriver driver) {
+		driver.quit();
+
+}
+}
