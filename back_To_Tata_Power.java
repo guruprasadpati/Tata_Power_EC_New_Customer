6f32d896794a9d3a6b8e603493417a057f2a9e40
guruprasad <guruprasadpati2@gmail.com>
2024-11-25 07:30:59 -0500

    Adding Println message

---
+++ back_To_Tata_Power.java
@@ -0,0 +1,11 @@
+package Tata_Power_Electric.Connection_for_New_Residential_Customer;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class back_To_Tata_Power {
+
+	public void clickButton(WebDriver driver) {
+		driver.findElement(By.xpath("//aside[text()='LT 3 PHASE CONNECTION (5.5KW TO 70KVA)']")).click();
+	}
+}
